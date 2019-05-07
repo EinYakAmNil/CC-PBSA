d6511f349147f073e8a0a2ae38bba3814d2ef314
EinYakAmNil <einyak@gmail.com>
2019-05-07 23:44:50 +0200

    Better class

--- classes_ccpbsa.py
+++ classes_ccpbsa.py
@@ -1,5 +1,7 @@
+"""Code for CC/PBSA, a fast tool for estimating mutational free energy
+differences.
+"""
 import os
-import sys
 import shutil
 import subprocess
 import numpy as np
@@ -7,6 +9,17 @@ import pandas as pd
 import pymol
 pymol.finish_launching(['pymol', '-qc'])
 cmd = pymol.cmd
+
+
+def makedir(dirname):
+    if not os.path.isdir(dirname):
+        os.mkdir(dirname)
+
+    else:
+        print("Directory \"%s\" already exists. Ignoring this function call!" %
+            dirname)
+        pass
+
 
 def unpack(lst):
 	"""Unpacks a nested list. Does not care about the deepness of nesting since
@@ -39,7 +52,7 @@ class Process_Logger:
         log = open(self.logname, 'a')
         log.write(proc_obj.stdout.decode('utf-8'))
         log.close()
-
+    
 
 class Mutater:
     """Takes a .pdb file and mutates one residue using PyMOL. Can take a .txt
@@ -64,13 +77,14 @@ class Mutater:
         cmd.wizard('mutagenesis')
 
         for m in range(len(self)):
+
             cmd.get_wizard().do_select('///%s/%s' %
-                    (self.chain[m], self.res[m]))
+                (self.chain[m], self.res[m]))
             cmd.get_wizard().set_mode(self.mut[m])
             cmd.get_wizard().apply()
             cmd.save("%s_%s.pdb" % (self.wt[:-4], self.mut_lst[m]))
             cmd.reinitialize()
-
+        
 
     def parse_mutations(self, txt):
         """Modifies the attributes of this object to make them passable to
@@ -86,7 +100,7 @@ class Mutater:
         aa3 = "ALA CYS ASP GLU PHE GLY HIS ILE LYS LEU \
             MET ASN PRO GLN ARG SER THR VAL TRP TYR".split()
         aa123 = dict(zip(aa1,aa3))
-
+        
         self.mut_lst = list(open(mutations_txt, 'r'))
         self.mut_lst = [mut.split('\n')[0] for mut in self.mut_lst]
         self.chain = [i[0] if '_' in i else '' for i in self.mut_lst.copy()]
@@ -101,7 +115,7 @@ class Mutater:
         """
         return len(self.mut_lst)
 
-
+        
 class FileOrganizer:
     """This class can split up GROMACS trajectory files into their seperate
     directories using \"gmx trjconv\". Also contains methods to search for
@@ -111,10 +125,10 @@ class FileOrganizer:
     def __init__(self, wd=os.getcwd()):
         self.cwd = wd
         self.files_split = 0
-
+        
         try:
             os.chdir(self.cwd)
-
+        
         except FileNotFoundError as err:
             raise type(err)("Could not change to directory \"%s\"" % self.cwd)
 
@@ -192,7 +206,7 @@ class FileOrganizer:
             os.chdir('..')
 
         return files
-
+        
 
     def parse_proc_flags(self, flags_raw):
         """Takes a file with flags for \"gmx\" and \"CONCOORD\" programs as
@@ -210,7 +224,7 @@ class FileOrganizer:
                 'MDRUN FLAGS': [list of flags],
             }
         }
-
+        
         The list can then be extended directly to other input lists for
         subprocess.run().
         """
@@ -237,22 +251,22 @@ class FileOrganizer:
         for line in content:
             if ';' in line:
                 line = line[:line.index(';')]
-
+    
             if len(line) > 0:
                 uncommented.append(' '.join(line.split()))
-
+    
     #    Indexes the file for where the flags are defined
         idx = [uncommented.index(j) for i in parsed_flags.values() for j in i]
         idx.append(len(uncommented))
-
+    
         i = 0
         for keys, vals in parsed_flags.items():
- 
+    
             for prog in vals:
-                parsed_flags[keys][prog] = unpack([x.split('=') for x in \
+                parsed_flags[keys][prog] = unpack([i.split('=') for i in \
                     uncommented[idx[i]+1:idx[i+1]]])
                 i += 1
-
+    
         return parsed_flags
 
 
@@ -260,17 +274,16 @@ class CC(Process_Logger):
     """Interacts with the programs of CONOCOORD (\"from CONstraints to
     COORDinates\"). Also parses the \"flags.txt\" file for gmx programs.
     """
-    def __init__(self, pdb_files, cc_flags):
+    def __init__(self, pdb_files, flags):
         """The attribute flag_file is the file containing the flags for all
         following programs. n_structures will be 0 until the \"concoord()\"
         method has been called.
         """
-        self.cc_flags = cc_flags
+        self.flags = flags
         self.n_structures = 0
         self.pdb_files = unpack([pdb_files])
         self.logname = "CONCOORD.log"
         self.start = os.getcwd()
-
 
     def concoord(self):
         """Performs the CONCOORD procedure to generate protein structure
@@ -281,18 +294,8 @@ class CC(Process_Logger):
         for f in self.pdb_files:
             fname = f[:-4]
             path = ('/'.join(fname.split('/')[:-1]))
-
-            if not path:
-                path = os.getcwd()
-                print(path)
-
-            try:
-                os.chdir(path)
-
-            except FileNotFoundError:
-                print("Working in directory: %s" % os.getcwd())
-                pass
-
+            os.chdir(path)
+    
             dist_input = [
                 'dist',
                 '-p', '%s' % f,
@@ -309,9 +312,9 @@ class CC(Process_Logger):
                 '-or', '%s_disco.rms' % fname,
                 '-of', '%s_disco_Bfac.pdb' % fname
             ]
-
-            dist_input.extend(self.cc_flags['DIST FLAGS'])
-            disco_input.extend(self.cc_flags['DISCO FLAGS'])
+            
+            dist_input.extend(self.flags['DIST FLAGS'])
+            disco_input.extend(self.flags['DISCO FLAGS'])
             self.n_structures = int(disco_input[disco_input.index('-n')+1])
     
             dist_proc = subprocess.run(
@@ -353,7 +356,7 @@ class CC(Process_Logger):
 
 class Energy_Minimizer(Process_Logger):
     """This class contains the functions and attributes to perform energy
-    minimizations on structrue (ensembles).
+    minimizations on structure (ensembles).
     """
     def __init__(
         self, pdb_files, mdp_file,
@@ -464,7 +467,6 @@ class LJ_Energy(Energy_Minimizer):
             '-o', 'FILENAME_energy.xvg'
         ]
 
-
     def calc_lj(self):
         """Calculate the Lennard-Jones energy (1-4 and SR) using the arguments to
         initialize this structure (ensemble). Uses the energy minimization
@@ -502,7 +504,6 @@ class Schlitter_Entropy(Process_Logger):
         """
         super().__init__(logname)
 
-
     def calc(self, xtc_file, topol_file):
         """Calculate the entropy according to Schlitter's formula
         """
@@ -524,14 +525,14 @@ class Schlitter_Entropy(Process_Logger):
             ]
         }
         proc = subprocess.run(entropy['covar_input'], input=b'0')
-        self.log(proc)
+        self.log(proc) 
         proc = subprocess.run(entropy['anaeig_input'], stdout=subprocess.PIPE)
         self.log(proc)
         schlitter = proc.stdout.decode('utf-8').split('\n')[0]
         schlitter = schlitter[schlitter.index('is ')+len('is ') \
             :schlitter.index(' J')]
         
-        return float(schlitter)/1000 # Output in kJ/mol
+        return float(schlitter)
         
 
     def concat(self, trr_files):
@@ -549,80 +550,281 @@ class Schlitter_Entropy(Process_Logger):
             raise Exception("Could not concatenate files. Check your files")
 
 
-class GXG(CC, LJ_Energy, Schlitter_Entropy):
-    """Contains the methods to create table to look up the energy differences.
+class Protein:
+    """Contains all the attributes and methods to create the CC/PBSA workflow.
+    Requires the programs \"CONCOORD\" and \"GROMACS 2019.2\". Python 3.7
+    module dependencies are numpy, pandas and python3-pymol.
     """
-    def __init__(self, flags):
-        self.x = list("ACDEFGHIKLMNPQRSTVWY")
-        self.cc_flags = flags['CC']
-        self.gmx_flags = flags['gmx']
-
-
-    def make_gxg(self):
-        """Make all 20 GXG tripeptides using PyMOL
+    def __init__(self, pdb_file, mut_list, flags,
+        min_mdp="/home/linkai/CC_PBSA/min.mdp",
+        energy_mdp="/home/linkai/CC_PBSA/energy.mdp", calculate='stability',
+        mdrun_table="/home/linkai/CC_PBSA/table4r-6-12.xvg"):
+        """Mandatory inputs:
+        - The protein in a .pdb file to be used.
+        - List of single amino acid (aa) mutations to intoduce to the protein
+        - A .txt file for mandatory and optional flags depending of the
+          program.
+        Optional inputs:
+        - Parameter files to be used for structure generation, energy
+          minimization, energy calculation, user-defined tables for \"gmx
+          mdrun\", surface area calculation and the entropy according to
+          Schlitter's formula.
+        - Mode of calculation: \"stability\" or \"affinity\".
+        Description of the attributes:
+        - mut_df is a pandas dataframe
         """
-        for aa in self.x:
-            cmd.fab('G%sG' % aa)
-            cmd.remove('hydrogens')
-            cmd.save('G%sG.pdb' % aa)
+        self.pdb_file = pdb_file
+        self.structures = [pdb_file]
+        self.name = pdb_file[:-4]
+        self.mut_df = self.parse_mutations(mut_list)
+        self.parsed_flags = self.parse_flags(flags)
+        self.n_structs = int(self.parsed_flags['CC']['DISCO FLAGS'] \
+                [self.parsed_flags['CC']['DISCO FLAGS'].index('-n')+1])
+        self.e_mdp = energy_mdp
+        self.min_mdp = min_mdp
+        self.mdrun_table = mdrun_table
+#        self.energy_df
+#        self.cpt
+        
+        makedir(self.name)
+        os.chdir(self.name)
+        makedir(self.name)
+        shutil.copy("../" + self.pdb_file, self.name)
+        self.maindir = os.getcwd()
+        
+
+    def __repr__(self):
+        """Returns the filename of the .pbd file. This string will also be used
+        for all protein mutations as prefix.
+        """
+        return self.name
+
+
+    def parse_flags(self, flags_raw):
+        """This function will be called upon object creation. Takes a file
+        with flags for \"gmx\" and \"CONCOORD\" programs as input. Returns a
+        dictionary in the following format:
+
+        parsed_flags = {
+            'CC': {
+                'DIST FLAGS': [list of flags],
+                'DISCO FLAGS': [list of flags]
+            },
+            'gmx': {
+                'PDB2GMX FLAGS': [list of flags],
+                'EDITCONF FLAGS': [list of flags],
+                'GROMPP FLAGS': [list of flags],
+                'MDRUN FLAGS': [list of flags],
+            }
+        }
+        
+        The list can then be extended directly to other input lists for
+        subprocess.run().
+        """
+        parsed_flags = {
+            'CC': {
+                'DIST FLAGS': [],
+                'DISCO FLAGS': []
+            },
+            'gmx': {
+                'PDB2GMX FLAGS': [],
+                'EDITCONF FLAGS': [],
+                'GROMPP FLAGS': [],
+                'MDRUN FLAGS': [],
+            }
+        }
+
+#        Search for this file just in case it is not in the current directory of
+#        the class.
+        flag_file = list(open(flags_raw, 'r'))
+        content = [line[:line.index('\n')] for line in flag_file]
+        uncommented = []
+
+        for line in content:
+            if ';' in line:
+                line = line[:line.index(';')]
+    
+            if len(line) > 0:
+                uncommented.append(' '.join(line.split()))
+    
+    #    Indexes the file for where the flags are defined
+        idx = [uncommented.index(j) for i in parsed_flags.values() for j in i]
+        idx.append(len(uncommented))
+    
+        i = 0
+        for keys, vals in parsed_flags.items():
+    
+            for prog in vals:
+                parsed_flags[keys][prog] = unpack([i.split('=') for i in \
+                    uncommented[idx[i]+1:idx[i+1]]])
+                i += 1
+    
+        return parsed_flags
+
+
+    def parse_mutations(self, txt):
+        """Modifies the attributes of this object to make them passable to
+        \"mutate()\".
+        """
+        def int_in_str(lst):
+            converted = ''.join([item for item in lst if item.isdigit()])
+            return int(converted)
+
+
+#        One letter AA code to three letter code as presented on PyMOL Wiki.
+        aa1 = list("ACDEFGHIKLMNPQRSTVWY")
+        aa3 = "ALA CYS ASP GLU PHE GLY HIS ILE LYS LEU \
+            MET ASN PRO GLN ARG SER THR VAL TRP TYR".split()
+        aa123 = dict(zip(aa1,aa3))
+
+        mut_lst = list(open(txt, 'r'))
+        mut_lst = [mut.split('\n')[0] for mut in mut_lst]
+
+#        Returned object is a pandas dataframe.
+        parsed_mut = pd.DataFrame(
+            columns=["Chain", "AA", "Residue", "Mutation"], index=mut_lst)
+
+        parsed_mut['Chain'] = [i[0] if '_' in i else '' for i in mut_lst.copy()]
+        parsed_mut['AA'] = [i[0] if '_' not in i else i[i.index('_')+1] \
+            for i in mut_lst.copy()]
+        parsed_mut['Residue'] = [int_in_str(i) for i in mut_lst]
+        parsed_mut['Mutation'] = [aa123[i[-1]] for i in mut_lst]
+
+        return parsed_mut
+
+    
+    def mutate(self):
+        """Uses PyMOL and the list of mutations to mutate the wildtype protein.
+        """
+        self.structures.extend(["{0}_{1}.pdb".format(
+            self, i) for i in self.mut_df.axes[0]])
+
+        for m in range(len(self.mut_df)):
+            cmd.load(self.name+'/'+self.pdb_file)
+            cmd.wizard('mutagenesis')
+            name = "%s_%s" % (self, self.mut_df.axes[0][m])
+            cmd.get_wizard().do_select('///%s/%s' %
+                (self.mut_df["Chain"][m], str(self.mut_df["Residue"][m])))
+            cmd.get_wizard().set_mode(self.mut_df["Mutation"][m])
+            cmd.get_wizard().apply()
+            cmd.save(name + ".pdb")
             cmd.reinitialize()
-
-
-    def create_table(self):
-        """Method used to create the tables of mutational energies for the GXG
-        tripeptides.
+            makedir(name)
+            shutil.move(name + ".pdb", name)
+        
+        
+    def log(self, fname, proc_obj):
+        """Used to log some of GROMACS output
         """
-        os.mkdir('testgxg')
-        os.chdir('testgxg')
-        self.make_gxg()
+        log_file = open(fname, 'a')
+        log_file.write(proc_obj.stdout.decode('utf-8'))
+        log_file.close()
 
-        for aa in self.x:
-            os.mkdir('G%sG' % aa)
-            shutil.move('G%sG.pdb' % aa, 'G%sG' % aa)
 
-        for aa in self.x:
-            os.chdir('G%sG' % aa)
-            self.pdb_files = 'G%sG.pdb' % aa
-            self.logname = 'CC_GXG.log'
-            self.concoord()
+    def concoord(self):
+        """Performs the CONCOORD procedure to generate protein structure
+        ensembles. Takes additional flags from "flag_parse" as input (pass it as
+        "flag_parse_output['CC']"). Make sure that \"CONCOORDRC.bash\" is
+        sourced.
+        """
+        for s in self.structures:
+            os.chdir(s[:-4])
+            dist_input = [
+                'dist',
+                '-p', '%s' % s,
+                '-op', '%s_dist.pdb' % s[:-4],
+                '-og', '%s_dist.gro' % s[:-4],
+                '-od', '%s_dist.dat' % s[:-4],
+            ]
+    
+            disco_input = [
+                'disco',
+                '-d', '%s_dist.dat' % s[:-4],
+                '-p', '%s_dist.pdb' % s[:-4],
+                '-op', '',
+                '-or', '%s_disco.rms' % s[:-4],
+                '-of', '%s_disco_Bfac.pdb' % s[:-4]
+            ]
+            dist_input.extend(self.parsed_flags['CC']['DIST FLAGS'])
+            disco_input.extend(self.parsed_flags['CC']['DISCO FLAGS'])
+            subprocess.run(dist_input, input=b'1\n1')
+            subprocess.run(disco_input)
+            
+            for n in range(1, self.n_structs+1):
+                nr = str(n)
+                makedir(nr)
+                shutil.move(nr+'.pdb', nr)
+
             os.chdir('..')
 
-        os.chdir('..')
+        self.structures = [s[:-4]+'/'+str(nr)+'/'+str(nr)+'.pdb' \
+            for s in self.structures for nr in range(1, self.n_structs+1)]
+
+    
+    def minimize(self, energy=False):
+        """Standard GROMACS routine to minimize a molecule to avoid bad
+        starting structures. If the argument \"energy\" is set to True, then
+        \"gmx grompp" will use by default a .mdp file for single point energy
+        evaluation without user supplied tables.
+        This method will by default be run on all files of the structures generated by
+        CONCOORD. The order in which to run those structures or adding/removing
+        structures can be achieved by modifying the attribute \"structures\".
+        """
+        if energy:
+            mdp = self.e_mdp
+
+        else:
+            mdp = self.min_mdp
+
+        for s in self.structures:
+            
+            if "/" in s:
+                os.chdir('/'.join(s.split('/')[:-1]))
+                struct = s.split('/')[-1][:-4]
+
+            else:
+                struct = s[:-4]
+
+            gmx_procedure = {
+                'pdb2gmx_input': [
+                    'gmx', '-quiet', 'pdb2gmx',
+                    '-f', '%s.pdb' % struct,
+                    '-o', '%s.gro' % struct,
+                    '-p', '%s.top' % struct
+            ],
+                'editconf_input': [
+                    'gmx', '-quiet', 'editconf',
+                    '-f', '%s.gro' % struct,
+                    '-o', '%s_box.pdb' % struct
+            ],
+                'grompp_input': [
+                    'gmx', '-quiet', 'grompp',
+                    '-f', mdp,
+                    '-c', '%s_box.pdb' % struct,
+                    '-p', '%s.top' % struct,
+                    '-o', '%s.tpr' % struct
+            ],
+                'mdrun_input': [
+                    'gmx', '-quiet', 'mdrun',
+                    '-s', '%s.tpr' % struct,
+                    '-deffnm', '%s_min' % struct,
+                    '-nt', '1',
+                    '-table', self.mdrun_table,
+                    '-tablep', self.mdrun_table
+            ]}
+
+            proc_count = 0
+            proc_ext = list(self.parsed_flags['gmx'].values())
+            
+            for proc in gmx_procedure.values():
+                proc_input = proc + proc_ext[proc_count]
+                subprocess.run(proc_input)
+                proc_count += 1
+
+            os.chdir(self.maindir)
 
 if __name__ == '__main__':
-
-#    if '-h' in sys.argv:
-#        help(sys.modules[__name__])
-#        exit()
-##    Valid flags: -p(rotein), -m(inimization mdp), -e(nergy mdp), -f(lags),
-##                 -mut(ations), -gxg, -h(elp)
-#    pdb_file = FileOrganizer().find(sys.argv[sys.argv.index('-p')+1])
-#    min_mdp = FileOrganizer().find(sys.argv[sys.argv.index('-m')+1])
-#    e_mdp = FileOrganizer().find(sys.argv[sys.argv.index('-e')+1])
-#    flags = FileOrganizer().find(sys.argv[sys.argv.index('-f')+1])
-#    mut_lst = FileOrganizer().find(sys.argv[sys.argv.index('-mut')+1])
-#    cc_flags = flags['CC']
-#    gmx_flags = flags['gmx']
-#    x = list("ACDEFGHIKLMNPQRSTVWY")
-##    Recalculate the GXG energy tables.
-#    if '-gxg' in cc_pbsa_flags:
-#        os.mkdir('GXG')
-#        os.chdir('GXG')
-#
-#        for aa in x:
-#            gxg = 'G%sG' % aa
-#            os.mkdir(gxg)
-#            os.chdir(gxg)
-#
-##            Use PyMOL to create the tripeptide.
-#            cmd.fab(gxg)
-#            cmd.remove('hydrogens')
-#            cmd.save('%s.pdb' % gxg)
-#            cmd.reinitialize()
-#
-##            CONCOORD creates the structure ensemble.
-#            CC('%s.pdb' % gxg, flags['CC']).concoord()
-#            os.chdir('..')
-    for i in range(20):
-        print(FileOrganizer('/Users/linkai/CC_PBSA/1pga/1pga').find(str(i)+'.pdb',
-            True))
+    x = Protein("1pga.pdb", "mut.txt", "param2.txt")
+    x.mutate()
+    x.concoord()
+    x.minimize()
